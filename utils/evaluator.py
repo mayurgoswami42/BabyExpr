e0db503d6c738781f459c15ddb880ca2851163dc
mayurgoswami42 <mayurgoswamimcpe3@gmail.com>
2026-07-15 15:18:41 +0530

    improved evaluator

--- utils/evaluator.py
+++ utils/evaluator.py
@@ -5,21 +5,22 @@ from .status_manager import StatusManager
 class Evaluator:
     def __init__(self, manager = None):
         self.manager = manager
-        if self.manager == None: self.manager = StatusManager()
+        if self.manager is None: self.manager = StatusManager()
         self.parser = Parser(self.manager)
         self.store = {"inf" : "infinite", "nan" : "undefined"}
         
     def evaluate(self, text = None, tree = None):
-        if tree == None:
+        if tree is None:
             tree = self.parser.construct_tree(text)
-        if type(tree) != BinaryOp:
+        if not isinstance(tree, BinaryOp):
             if tree[1] == Token.NUMBER:
                 return float(tree[0])
             elif tree[1] == Token.IDENTIFIER:
                 if tree[0] in self.store.keys():
                     try:
                         return float(self.store[tree[0]])
-                    except:
+                    except Exception as e:
+                        self.manager.throwE(f"EVALUATOR::ERROR:: Exception \"{e}\"")
                         return self.store[tree[0]]
                 else:
                     self.manager.throwE(f"EVALUATOR::ERROR:: Undefined variable \"{tree[0]}\"")
@@ -27,25 +28,27 @@ class Evaluator:
             else:
                 self.manager.throwE(f"EVALUATOR::ERROR:: Bad Expression")
                 return 0
+
+        left_side = self.evaluate(tree = tree.left)
+        right_side = self.evaluate(tree = tree.right)
         
-        if (type(tree.left) != BinaryOp and len(tree.left) == 0) or (type(tree.right) != BinaryOp and len(tree.right) == 0):
+        if (not isinstance(tree.left, BinaryOp) and len(tree.left) == 0) or (not isinstance(tree.right, BinaryOp) and len(tree.right) == 0):
             return 0
         if tree.op[1] == Token.PLUS:
-            return self.evaluate(tree = tree.left) + self.evaluate(tree = tree.right)
+            return left_side + right_side
         elif tree.op[1] == Token.MINUS:
-            return self.evaluate(tree = tree.left) - self.evaluate(tree = tree.right)
+            return left_side - right_side
         elif tree.op[1] == Token.MUL:
-            return self.evaluate(tree = tree.left) * self.evaluate(tree = tree.right)
+            return left_side * right_side
         elif tree.op[1] == Token.DIV:
-            right_side = self.evaluate(tree = tree.right)
             if right_side == 0:
                 self.manager.throwE("EVALUATOR::ERROR:: Can't divide by 0!")
                 return 0
             else:
-                return self.evaluate(tree = tree.left) / self.evaluate(tree = tree.right)
+                return left_side / right_side
         elif tree.op[1] == Token.EQUAL:
-            if type(tree.left) != BinaryOp and tree.left[1] == Token.IDENTIFIER:
-                self.store[tree.left[0]] = self.evaluate(tree = tree.right)
+            if not isinstance(tree.left, BinaryOp) and tree.left[1] == Token.IDENTIFIER:
+                self.store[tree.left[0]] = right_side
                 return self.store[tree.left[0]]
             else:
                 self.manager.throwE("EVALUATOR::ERROR:: Bad expression lvalue expexted before = ")
